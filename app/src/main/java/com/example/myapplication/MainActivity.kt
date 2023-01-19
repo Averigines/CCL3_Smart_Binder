d355a8fd50081e31c6f2ea36b8122d42772901d6
cc211036 <cc211036@hotmail.fr>
2023-01-19 13:58:36 +0100

    fix: gradle

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.example.myapplication
 
-import android.os.Build
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.annotation.RequiresApi
-import com.theokanning.openai.OpenAiResponse
-import com.theokanning.openai.completion.CompletionRequest
-//openai-java/client/src/main/java/com/theokanning/openai/OpenAiService.java
-//openai-java/api/src/main/java/com/theokanning/openai/completion/CompletionRequest.java
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,14 +10,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-    //val service = OpenAiService("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")
-    val completionRequest = CompletionRequest.builder()
-        .prompt("hello")
-        .model("ada")
-        .echo(true)
-        .build()
-    //service.createCompletion(completionRequest).getChoices().forEach(System.ou::println
 
 
 }
