c21d53a1da0b7aadfd63b3e6d24b909b0d65ba99
Patrick <cc211032@fhstp.ac.at>
2023-01-19 14:28:22 +0100

    Added some design and preview of left/right topic with cards

--- app/src/main/java/com/example/myapplication/ActivityCategory.kt
+++ app/src/main/java/com/example/myapplication/ActivityCategory.kt
@@ -1,7 +1,11 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.DimenRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
@@ -9,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import java.lang.Math.abs
 
 class TopicPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle, private val topicsWithCards: List<TopicsWithCards>) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
@@ -59,10 +64,37 @@ class ActivityCategory : AppCompatActivity() {
         //viewPager.adapter = TopicPagerAdapter(supportFragmentManager, lifecycle, topics, cards)
         viewPager.adapter = TopicPagerAdapter(supportFragmentManager, lifecycle, topicsWithCards)
         viewPager.currentItem = activeTopicId
+
+        viewPager.offscreenPageLimit = 1
+        val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
+        val currentItemHorizontalMarginPx = resources.getDimension(R.dimen.viewpager_current_item_horizontal_margin)
+        val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
+        val pageTransformer = ViewPager2.PageTransformer { page: View, position: Float ->
+            page.translationX = -pageTranslationX * position
+            page.alpha = 0.25f + (1 - kotlin.math.abs(position))
+        }
+        viewPager.setPageTransformer(pageTransformer)
+        val itemDecoration = HorizontalMarginItemDecoration(this, R.dimen.viewpager_current_item_horizontal_margin
+        )
+        viewPager.addItemDecoration(itemDecoration)
         //val rvCards = findViewById<RecyclerView>(R.id.rvCards)
         //rvCards.adapter = CardAdapter(cards)
         //viewPager.registerOnPageChangeCallback(ViewPagerCallback(rvCards, topics, db))
 
+    }
+}
+
+class HorizontalMarginItemDecoration(context: Context, @DimenRes horizontalMarginInDp: Int) :
+    RecyclerView.ItemDecoration() {
+
+    private val horizontalMarginInPx: Int =
+        context.resources.getDimension(horizontalMarginInDp).toInt()
+
+    override fun getItemOffsets(
+        outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State
+    ) {
+        outRect.right = horizontalMarginInPx
+        outRect.left = horizontalMarginInPx
     }
 
 }
