ddfe73b43d178490e77080203374e3106c137273
cc211036 <cc211036@hotmail.fr>
2023-01-25 14:55:10 +0100

    add: category selection activity

--- app/src/main/java/com/example/myapplication/FlipCard.kt
+++ app/src/main/java/com/example/myapplication/FlipCard.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication
 
-import Cards
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.MotionEvent
@@ -9,12 +8,15 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
-import androidx.core.view.get
+import androidx.core.view.size
 import androidx.room.Room
 import androidx.viewpager2.widget.ViewPager2
+import java.lang.Integer.min
 
 
 private lateinit var cardsViewPager: ViewPager2
+
+data class ViewsList()
 
 class FlipCard : AppCompatActivity() {
 
@@ -29,11 +31,21 @@ class FlipCard : AppCompatActivity() {
             AppDatabase::class.java, "smartBinderDB"
         ).allowMainThreadQueries().build()
 
-        val cardList: List<Card>  = db.cardDao().getAll() as MutableList<Card>
-        val numberOfElement: Int = 5
-        val randomElements = cardList.asSequence().shuffled().take(numberOfElement).toList()
+        val selectedCategory: String = intent.getStringExtra("selectedCategory") as String
 
-        println(randomElements)
+        var cardsList: List<Card> = listOf()
+
+        cardsList =
+            if (selectedCategory.toString() == "All categories") {
+                db.cardDao().getAll()
+            } else {
+                db.cardDao().getAllCardsOfCategory(selectedCategory)
+            }
+
+        var numberOfElement: Int = min(5, cardsList.size)
+        val randomListOf5 = cardsList.asSequence().shuffled().take(numberOfElement).toList()
+
+        println(randomListOf5)
 
         val leftCornerGradient: ImageView = findViewById(R.id.gradient_left)
         val rightCornerGradient: ImageView = findViewById(R.id.gradient_right)
@@ -41,9 +53,21 @@ class FlipCard : AppCompatActivity() {
         val rightCornerText: TextView = findViewById(R.id.tv_rightGradient)
         val infoBtn: TextView = findViewById(R.id.tv_contentCheck)
         val cardInfo: CardView = findViewById(R.id.cv_cardInfo)
-
+        val dao = db.topicDao()
+        val daoCategory = db.categoryDao()
+        val tvCategory: TextView = findViewById(R.id.tv_catNameOnFlip)
         val viewPagerAdapter =
-            DetailViewPagerAdapter(randomElements, leftCornerGradient, rightCornerGradient, leftCornerText, rightCornerText)
+            DetailViewPagerAdapter(
+                randomListOf5,
+                leftCornerGradient,
+                rightCornerGradient,
+                leftCornerText,
+                rightCornerText,
+                dao,
+                daoCategory,
+                tvCategory,
+                infoBtn,
+            )
         cardsViewPager.adapter = viewPagerAdapter
 
         cardsViewPager.isUserInputEnabled = false
@@ -61,13 +85,16 @@ class FlipCard : AppCompatActivity() {
                         cardInfo.visibility = View.GONE
                         return@setOnTouchListener true
                     }
-                    else -> {return@setOnTouchListener false}
+                    else -> {
+                        return@setOnTouchListener false
+                    }
                 }
             }
         }
 
         cardsViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
+
             }
         })
 
